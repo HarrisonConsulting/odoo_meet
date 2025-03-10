b4dd5b62573a9fb0d7cda3c83a2295a308cec9e3
Matt Harrison <matt@harrison.consulting>
2025-03-10 03:19:06 -0400

    Bump version to 17.0.0.0.2 and update author name in Google Meet integration manifest

--- google_meet/__manifest__.py
+++ google_meet/__manifest__.py
@@ -1,13 +1,13 @@
 {
     "name": "Google Meet",
-    "version": '17.0.0.0.1',
+    "version": '17.0.0.0.2',
     "summary": "Integrates Google Meet Video Conferencing with Odoo Calendar Events",
     "description": """ 
         This module integrates Google Meet Video Conferencing with Odoo Calendar Events.
         It includes the necessary files and configurations to enable Google Meet for appointments when the location is left empty.
     """,
     "category": "Website",
-    "author": "Harrison Consulting, LLC",
+    "author": "Harrison Consulting",
     "website": "https://www.harrison.consulting",
     'license': 'GPL-3',
     "depends": ["calendar", "google_calendar"],
